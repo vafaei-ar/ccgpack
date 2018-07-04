1320815b5c64b5e570a8366d0c3e2b9d66540bd6
vafaei-ar <you@example.com>
2018-07-04 16:44:48 +0430

    readme

--- ccgpack/curvelet.py
+++ ccgpack/curvelet.py
@@ -6,7 +6,7 @@ path = '/'.join(os.path.abspath(__file__).split('/')[:-1])+'/'
 ctypes.cdll.LoadLibrary(path+'/cpp_src/fftw-2.1.5/fftw/.libs/libfftw.so.2')
 curlib = ctypes.cdll.LoadLibrary(path+'/cpp_src/curvelet.so')
 
-def curvelet(m,n_scales,r_scale,n_wedges,ac):
+def curvelet(m,r_scale,n_scales=7,n_wedges=10,ac=1):
     m = np.array(m, dtype=np.double)
     nx = m.shape[0]
     ny = m.shape[1]
