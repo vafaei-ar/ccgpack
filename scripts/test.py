78ae3afaa1ac8b06209a489abcdb4a19e7433585
vafaei-ar <you@example.com>
2018-07-05 17:18:08 +0430

    test

--- scripts/test.py
+++ scripts/test.py
@@ -2,16 +2,16 @@
 
 import ccgpack as ccg
 import numpy as np
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 
 from scipy import misc
 m = misc.imread('../images/einstein.jpg')
 
 cm = ccg.curvelet(m,7,4,10,1)
 
-plt.subplot(1,2,1)
-plt.imshow(m)
-plt.subplot(1,2,2)
-plt.imshow(cm)
-plt.show()
+#plt.subplot(1,2,1)
+#plt.imshow(m)
+#plt.subplot(1,2,2)
+#plt.imshow(cm)
+#plt.show()
 
