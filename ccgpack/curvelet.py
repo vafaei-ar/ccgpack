da9480b244c7a982bdf5fa2a0fe925f46d571df9
vafaei-ar <you@example.com>
2018-07-05 12:50:52 +0430

    test_version

--- ccgpack/curvelet.py
+++ ccgpack/curvelet.py
@@ -5,6 +5,10 @@ import os
 path = '/'.join(os.path.abspath(__file__).split('/')[:-1])
 fftw_path = '/cpp_src/fftw-2.1.5/fftw/.libs/libfftw.so.2'
 crv_path = '/cpp_src/curvelet.so'
+
+print os.path.join(path,fftw_path)
+print os.path.join(path,crv_path)
+
 ctypes.cdll.LoadLibrary(os.path.join(path,fftw_path))
 curlib = ctypes.cdll.LoadLibrary(os.path.join(path,crv_path))
 
