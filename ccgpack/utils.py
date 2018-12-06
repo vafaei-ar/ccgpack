9c57ae0b9c35223a9f3710d09e5ca555930b9d4d
gf <vafaei_ar@yahoo.com>
2018-12-06 10:57:02 +0330

    download function

--- ccgpack/utils.py
+++ ccgpack/utils.py
@@ -7,9 +7,44 @@ import sys
 import math
 import pprint
 import random
+import urllib
+import requests
 from time import gmtime, strftime
 
 
+def connected(url='http://www.google.com/'):
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("Internet connection problem.")
+        return False
+
+#def internet_check():
+#    try:
+#        urllib.urlopen('http://216.58.192.142')
+#        return True
+#    except urllib2.URLError as err: 
+#        return False
+
+def report(count, blockSize, totalSize):
+  	percent = int(count*blockSize*100/(totalSize))
+  	sys.stdout.write("\r%d%%" % percent + ' complete')
+  	sys.stdout.flush()
+
+def download(getFile, saveFile=None):
+    assert connected(),'Error! check your Internet connection.'
+    if saveFile is None:
+        saveFile = getFile.split('/')[-1]
+    sys.stdout.write('\rFetching ' + saveFile + '...\n')
+    try:
+        urllib.urlretrieve(getFile, saveFile, reporthook=report)
+    except:
+        urllib.request.urlretrieve(getFile, saveFile, reporthook=report)
+    sys.stdout.write("\rDownload complete, saved as %s" % (saveFile) + '\n\n')
+    sys.stdout.flush()
+    
 def ch_mkdir(directory):
     """
     ch_mkdir : This function creates a directory if it does not exist.
